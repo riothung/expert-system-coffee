e72e8b47f8eb37c9e968286ecd874334ae7b5c34
riothung <mario.thung@gmail.com>
2025-02-19 05:33:28 +0800

    first commit, seperate frontend and backend, add supabase, add web server, add prisma

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const compression = require("compression");
+const cookieParser = require("cookie-parser");
+
+const router = require("./routes/index");
+
+const app = express();
+
+const PORT = 3000;
+
+dotenv.config();
+app.use(helmet());
+app.use(compression());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+  })
+);
+
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "50mb",
+  })
+);
+
+app.use("/api", router);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening in port ${PORT}`);
+});
